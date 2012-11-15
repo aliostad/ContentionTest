506b7b55a9e6b093bf22f551c6fd900e1e60d6fa
Ali Kheyrollahi <aliostad@gmail.com>
2012-11-15 18:04:53 +0000

    Initial commit

---
+++ src/ContentionTest/ContentionWriter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ContentionTest
+{
+    public class ContentionWriter : IDisposable
+    {
+
+        private string _fileName = string.Empty;
+        private int _howMuch;
+        private Stream _stream;
+        private Stopwatch _stopwatch = new Stopwatch();
+   
+
+        public ContentionWriter(int howMuch)
+        {
+            _howMuch = howMuch;
+        }
+
+        public Stopwatch Stopwatch
+        {
+            get { return _stopwatch; }
+            set { _stopwatch = value; }
+        }
+
+        public Task WriteAsync(byte[] buffer)
+        {
+            //Thread.Sleep(100);
+            var random = new Random();
+            _fileName = GetFileName();
+            _stream = new FileStream(_fileName, FileMode.Create);
+            _stopwatch.Start();
+            return Task.Factory.FromAsync(_stream.BeginWrite, _stream.EndWrite, buffer, random.Next(_howMuch), _howMuch, null);
+            //return Task.Factory.StartNew(() => _stream.Write(buffer, random.Next(_howMuch), _howMuch));
+        }
+
+        public void Write(byte[] buffer)
+        {
+            var random = new Random();
+            _fileName = GetFileName();
+            _stream = new FileStream(_fileName, FileMode.Create);
+            _stream.Write(buffer, random.Next(_howMuch), _howMuch);
+        }
+
+        private string GetFileName()
+        {
+            //return Path.GetTempFileName();
+            //return "k:\\" + Guid.NewGuid().ToString("N");
+            return "c:\\temp\\" + Guid.NewGuid().ToString("N");
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+            if(!string.IsNullOrEmpty(_fileName))
+                File.Delete(_fileName);
+
+        }
+    }
+}
---
+++ src/ContentionTest/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ContentionTest
+{
+    class Program
+    {
+        static int total = 0;
+
+        static void Main(string[] args)
+        {
+            bool async = false;
+            const int n = 100000;
+
+            ThreadPool.SetMaxThreads(50, 100);
+            ThreadPool.SetMinThreads(50, 100);
+
+            byte[] buffer = new byte[10000 * 1024];
+            Random random = new Random();
+            random.NextBytes(buffer);
+
+            int thcount = 0;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < n; i++)
+            {
+                //Thread.Sleep(10);
+                if (!async)
+                {
+                    Console.Write("\r" + i);                    
+                }
+                var writer = new ContentionWriter(10*1024);
+                
+                if(async)
+                    writer.WriteAsync(buffer)
+                        .ContinueWith((t) =>
+                        {
+                            writer.Stopwatch.Stop();
+                            Console.Write("\r" + writer.Stopwatch.Elapsed + "(" + i + ") ");
+                            writer.Dispose();
+                            Interlocked.Increment(ref total);
+                        });
+                else
+                {
+                    writer.Write(buffer);   
+                    writer.Dispose();
+                }
+            }
+
+
+            if(async)
+            {
+                while (total < n - 1)
+                {
+                    Thread.Sleep(200);
+                }
+            }
+
+         
+
+
+            stopwatch.Stop();
+            Console.WriteLine();            
+            Console.WriteLine(stopwatch.Elapsed);
+
+            Console.Read();
+        }
+    }
+}
